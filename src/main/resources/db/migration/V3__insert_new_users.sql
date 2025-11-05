cd72a6a1cc6539e381150aa6dbb35fdd3219e277
unknown <giovannarev.roz@gmail.com>
2025-11-05 11:41:38 -0300

    alterando migrations

--- src/main/resources/db/migration/V3__insert_new_users.sql
+++ src/main/resources/db/migration/V3__insert_new_users.sql
@@ -1,5 +1,15 @@
+-- Inserir ou atualizar usuário kgonascimento
 INSERT INTO USUARIO (username, password, role)
-VALUES ('kgonascimento', '$2a$10$9j7OWhYxUJS3lzehf9tbyuBCTsPJmZYIXRuH.z0.a8Iw5wDvtJkPC', 'ROLE_FUNCIONARIO');
+VALUES ('kgonascimento', '$2a$10$9j7OWhYxUJS3lzehf9tbyuBCTsPJmZYIXRuH.z0.a8Iw5wDvtJkPC', 'ROLE_FUNCIONARIO')
+ON CONFLICT (username)
+DO UPDATE SET
+    password = EXCLUDED.password,
+    role = EXCLUDED.role;
 
+-- Inserir ou atualizar usuário admin
 INSERT INTO USUARIO (username, password, role)
-VALUES ('admin', '$2a$10$GDnclieRR2G76y06Em4PoexO4xm.08cRMB0TJe1jIFgXLIS3xiO8.', 'ROLE_GERENTE');
+VALUES ('admin', '$2a$10$GDnclieRR2G76y06Em4PoexO4xm.08cRMB0TJe1jIFgXLIS3xiO8.', 'ROLE_GERENTE')
+ON CONFLICT (username)
+DO UPDATE SET
+    password = EXCLUDED.password,
+    role = EXCLUDED.role;
